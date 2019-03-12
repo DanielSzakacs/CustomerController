a015d9254a3fb3870ad6a57bc863b44f2c174409
danielszakacs <daniel.sokach@gmail.com>
2019-03-12 12:25:26 +0100

    Admin class methods created. Ready for testing.

--- src/main/java/com/danielszakacs/customer/controller/customercontroller/DAO/module/Admin.java
+++ src/main/java/com/danielszakacs/customer/controller/customercontroller/DAO/module/Admin.java
@@ -1,4 +1,32 @@
 package com.danielszakacs.customer.controller.customercontroller.DAO.module;
 
+import javax.persistence.*;
+
+@Entity
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String adminEmail;
+
+    private String password;
+
+    public Admin(String adminEmail, String password) {
+        this.adminEmail = adminEmail;
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
 }
