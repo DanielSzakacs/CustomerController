1292bbeb78bcbf4e3ba86a902b6547aef82b9b48
danielszakacs <daniel.sokach@gmail.com>
2019-03-12 11:55:09 +0100

    Customer class methods are created.

--- src/main/java/com/danielszakacs/customer/controller/customercontroller/DAO/module/Customer.java
+++ src/main/java/com/danielszakacs/customer/controller/customercontroller/DAO/module/Customer.java
@@ -1,4 +1,64 @@
 package com.danielszakacs.customer.controller.customercontroller.DAO.module;
 
+import javax.persistence.*;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "customerName")
+    private String name;
+
+    private String email;
+
+    private String address;
+
+    private int telephone;
+
+    public Customer(String name, String email, String address, int telephone) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.telephone = telephone;
+    }
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
 }
