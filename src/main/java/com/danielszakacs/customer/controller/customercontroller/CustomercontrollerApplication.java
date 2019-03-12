fc038f8736490a75e3528533a75f3217fea2615a
danielszakacs <daniel.sokach@gmail.com>
2019-03-12 10:47:47 +0100

    git common file added.

---
+++ src/main/java/com/danielszakacs/customer/controller/customercontroller/CustomercontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.danielszakacs.customer.controller.customercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomercontrollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomercontrollerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danielszakacs/customer/controller/customercontroller/CustomercontrollerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.danielszakacs.customer.controller.customercontroller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomercontrollerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
